f9e3c371904e3a9ac91305ce402764bc174e84af
TrungKiennef <109646424+TrungKiennef@users.noreply.github.com>
2022-08-15 19:49:05 +0700

    Create Exercise 1.cpp

---
+++ Exercise 1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int main() {
+   char s1[50];     
+   char s2[50];
+    
+   int n = 0;
+   short flag = 1;
+    
+    printf("Nhap chuoi s1: ");
+    gets(s1);
+    printf("Nhap chuoi s2: ");
+    gets(s2);
+     
+   while (s1[n] != '\0') {
+      if(s1[n] != s2[n]) {
+         flag = 0;
+         break;
+      }
+      n++;
+   }
+     
+   if(flag == 1) {
+      printf("Chuoi %s va chuoi %s khong nam trong nhau.", s1, s2);
+   }else {
+      printf("Chuoi %s va chuoi %s  nam trong nhau.", s1, s2);
+   }
+ 
+   return 0;
+}
